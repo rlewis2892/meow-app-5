cf233b89510a417ce2a06013c466a7eacc0f9702
Rochelle Lewis <rlewis37@gmail.com>
2017-11-30 17:47:06 -0700

    install angular, add base files for app

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
+	selector: 'meow-app',
+
+	// templateUrl path to your public_html/templates directory. .html files only.
+	templateUrl: './templates/meow-app.html'
+})
+
+export class AppComponent {}

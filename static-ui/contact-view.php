bdf8fe06c59de1d94e0928b99223c1239592913b
Rochelle Lewis <rlewis37@gmail.com>
2017-12-02 19:26:31 -0700

    finalize static-ui

--- static-ui/contact-view.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rochelle
- * Date: 11/30/17
- * Time: 5:49 PM
- */
---
+++ static-ui/posts-view.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<?php require_once ("head-utils.php");?>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<?php require_once("navbar.php");?>
+
+			<main class="my-5">
+				<div class="container-fluid text-center text-sm-left">
+
+					<div class="row mb-3">
+						<div class="col">
+							<h1>Meow Forum</h1>
+						</div>
+					</div>
+
+					<div class="card-columns">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card title that wraps to a new line</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							</div>
+						</div>
+						<div class="card p-3">
+							<blockquote class="blockquote mb-0 card-body">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+								<footer class="blockquote-footer">
+									<small class="text-muted">
+										Someone famous in <cite title="Source Title">Source Title</cite>
+									</small>
+								</footer>
+							</blockquote>
+						</div>
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+								<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+							</div>
+						</div>
+						<div class="card bg-primary text-white text-center p-3">
+							<blockquote class="blockquote mb-0">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+								<footer class="blockquote-footer">
+									<small>
+										Someone famous in <cite title="Source Title">Source Title</cite>
+									</small>
+								</footer>
+							</blockquote>
+						</div>
+						<div class="card text-center">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+								<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+							</div>
+						</div>
+						<div class="card p-3 text-right">
+							<blockquote class="blockquote mb-0">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+								<footer class="blockquote-footer">
+									<small class="text-muted">
+										Someone famous in <cite title="Source Title">Source Title</cite>
+									</small>
+								</footer>
+							</blockquote>
+						</div>
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+							</div>
+						</div>
+					</div>
+
+					<!--<div class="row">
+
+						<div class="col-md-6">
+							<h1>Hello, Miau!</h1>
+						</div>
+
+						<div class="col-md-6">
+							<div class="card bg-shadow-light border-0 rounded-0 text-white text-shadow-dark">
+								<div class="card-body">
+									<h4>We specialize in extraordinary meow experiences.</h4>
+									<hr>
+									<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+									<p>Scratch the furniture put butt in owner's face rub whiskers on bare skin act innocent or roll over and sun my belly brown cats with pink ears or step on your keyboard while you're gaming and then turn in a circle meowwww. If it fits, i sits mark territory, but scamper stare at ceiling i shredded your linens for you kick up litter yet curl into a furry donut. Leave dead animals as gifts lounge in doorway for ooh, are those your $250 dollar sandals?</p>
+									<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+								</div>
+							</div>
+
+							<div class="my-2 text-right">
+								<span class="font-weight-light font-italic">Don't have an account?</span><button class="btn-link py-0 text-white border-0 font-weight-bold" data-toggle="modal" data-target="#signUpModal">Sign up today!</button>
+							</div>
+						</div>
+
+					</div>--><!--/.row-->
+				</div><!--/.container-fluid-->
+			</main>
+
+		</div>
+
+		<?php require_once("footer.php");?>
+
+		<?php require_once("sign-up-modal.php");?>
+
+	</body>
+</html>

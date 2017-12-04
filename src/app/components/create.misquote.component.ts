bea6ce10b78265ac9136c9d81a8e4be0838e9775
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-04 14:35:23 -0700

    added create misquote component

---
+++ src/app/components/create.misquote.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "create-misquote",
+	templateUrl: "./templates/create.misquote.html"
+})
+
+export class CreateMisquoteComponent {}

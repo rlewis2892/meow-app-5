e220b9cd90ce2c86bbb56e10f6a76af09e837666
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-04 14:07:01 -0700

    finalized misquote component

--- src/app/components/misquote.component.ts
+++ src/app/components/misquote.component.ts
@@ -1,7 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {MisquoteService} from "../services/misquote.service";
+import {ActivatedRoute} from "@angular/router";
+import {Misquote} from "../classes/misquote";
 
 @Component({
 	templateUrl: "./templates/misquote.html"
 })
 
-export class MisquoteComponent {}
+export class MisquoteComponent implements OnInit {
+
+	misquote: Misquote = new Misquote(null, null, null, null);
+
+	constructor(private misquoteService: MisquoteService, private route: ActivatedRoute) {}
+
+	getMisquoteByMisquoteId() : void {
+		let misquoteId : string = this.route.snapshot.params["misquoteId"];
+		this.misquoteService.getMisquote(misquoteId)
+			.subscribe(misquote => this.misquote = misquote);
+	}
+
+	ngOnInit() : void {
+		this.getMisquoteByMisquoteId();
+	}
+}

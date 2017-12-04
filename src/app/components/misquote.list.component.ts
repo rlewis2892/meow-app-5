23adb069ea4aaa511437c5bfee45eaac0a92ef32
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-04 13:30:34 -0700

    added init contract to component

--- src/app/components/misquote.list.component.ts
+++ src/app/components/misquote.list.component.ts
@@ -1,7 +1,23 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {MisquoteService} from "../services/misquote.service";
+import {Misquote} from "../classes/misquote";
 
 @Component({
 	templateUrl: "./templates/misquote.list.html"
 })
 
-export class MisquoteListComponent {}
+export class MisquoteListComponent implements OnInit {
+
+	misquotes: Misquote[] = [];
+
+	constructor(private misquoteService: MisquoteService) {}
+
+	getAllMisquotes(): void {
+		this.misquoteService.getAllMisquotes()
+			.subscribe(misquotes => this.misquotes = misquotes);
+	}
+
+	ngOnInit(): void {
+		this.getAllMisquotes();
+	}
+}
